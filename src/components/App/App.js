8084e3cfd7de0c06279dd37a7c7f877e3a1dd130
Brian <brianjmorrissey@gmail.com>
2021-04-15 15:07:50 -0500

    third portion of project completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,14 +21,19 @@ const business = {
 const businesses = [business, business, business, business, business, business];
 
 
+
 class App extends React.Component {
+
+  searchYelp(term, location, sortBy) {
+    console.log("Searching Yelp with Pizza, Brooklyn, best_match")
+  };
   render() {
     return (
       <div className="App">
-  <h1>ravenous</h1>
-  <SearchBar />
-  <BusinessList businesses={businesses}/>
-</div>
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={businesses}/>
+      </div>
     );
   }
 }
